02bac19dab3d8917076e626de77287ca31cfa921
NITHYA REDDY RAGULA <85093952+ragulanithyareddy@users.noreply.github.com>
2022-01-22 13:24:16 +0530

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,3 +1,10 @@
 import random
 
-x
+x= int(input(" enter 0 to roll "))
+
+while(x==0):
+
+ if(x==0):
+     y= int(random.randint(1,6))
+     print ("______\n|     |\n| ",y," |\n|_____|")
+     x= int(input(" enter 0 to roll "))
